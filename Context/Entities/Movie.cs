e04e1394bc6cca18d2a2783d7df1c34b9afc2444
Csilla Ozsvath <c.ozsvath@resource-it.ro>
2017-06-20 14:19:05 +0300

    - Added Movie Entity

---
+++ Context/Entities/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUDApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
