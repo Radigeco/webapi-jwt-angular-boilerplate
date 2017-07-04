2b76ad46b0794047834d69bfd76d463b93eec35a
Attila Gal <a.gal@resource-it.ro>
2017-07-04 11:42:12 +0300

    - added upload file to web api sample

---
+++ Infrastructure/Helpers/MultiPartFileHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace Infrastructure.Helpers
+{
+    public static class MultipartFileHelper
+    {
+        public static string GetDeserializedFileName(MultipartFileData fileData)
+        {
+            var fileName = GetFileName(fileData);
+            return JsonConvert.DeserializeObject(fileName).ToString();
+        }
+
+        private static string GetFileName(MultipartFileData fileData)
+        {
+            return fileData.Headers.ContentDisposition.FileName;
+        }
+    }
+}
---
+++ Websolution/Controllers/FileController.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Infrastructure.Helpers;
+using Security.Providers;
+using Websolution.Controllers.Base;
+
+namespace Websolution.Controllers
+{
+    [RoutePrefix("api/file")]
+    public class FileController : BaseApiController
+    {
+
+        [Route("upload")]
+        [HttpPost]
+        public async Task<HttpResponseMessage> UploadFile()
+        {
+
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                return new HttpResponseMessage(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            string root = HttpContext.Current.Server.MapPath("~/App_Data");
+            CustomMultipartFormDataStreamProvider provider = new CustomMultipartFormDataStreamProvider(root);
+            CustomMultipartFormDataStreamProvider result = await Request.Content.ReadAsMultipartAsync(provider);
+
+            foreach (MultipartFileData file in provider.FileData)
+            {
+                string originalFileName = MultipartFileHelper.GetDeserializedFileName(result.FileData.First());
+
+                using (FileStream stream = new FileStream(file.LocalFileName, FileMode.Open))
+                {
+                    //Do something with the stream
+                    stream.Close();
+                    stream.Dispose();
+                }
+            }
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
